38bfc1a85325ccebebf1e2c4deddb653dd381127
mwdoyle-sfu <mwdoyle@sfu.ca>
2019-01-19 12:57:30 -0800

    deploy node to heroku

--- server.js
+++ server.js
@@ -1,18 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
 const app = express()
+
+const apiKey = '3b3f81e245b0f348658d3fe2c7a02946';
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('index');
+  res.render('index', {weather: null, error: null});
 })
+
 app.post('/', function (req, res) {
-  console.log(req.body.city);
-  res.render('index');  
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
 })
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
